277a092be80bc811e48383d63b88366859c1c8c5
David Leiva <david.leiva04@gmail.com>
2019-07-04 23:56:04 -0600

    Next init and index route

---
+++ app/layouts/page.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+// import Header from "../components/Layout/Header";
+
+// import "../scss/style.scss";
+
+const Page = ({ children }) => (
+  <Fragment>
+    <main>{children}</main>
+  </Fragment>
+);
+
+export default Page;
---
+++ server/server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const session = require("express-session");
+const uuid = require("uuid");
+const {
+     parse
+} = require("url");
+
+const next = require("next");
+
+// Set Environment
+const dev = process.env.NODE_ENV || "development";
+const PORT = process.env.PORT || 3000;
+
+// Next App
+const app = next({
+     dir: "./app",
+     dev
+});
+const handle = app.getRequestHandler();
+
+// Sequelize
+// const models = require("./models");
+
+/**
+ * Front end Routes
+ */
+const getRoutes = require("./routes");
+const routes = getRoutes();
+
+app
+     .prepare()
+     .then(() => {
+          const server = express();
+
+          // Generate Unique Session Secret tokens
+          server.use(
+               session({
+                    secret: uuid.v1(),
+                    name: "sessionId",
+                    resave: true,
+                    saveUninitialized: true
+               })
+          );
+
+          server.use(express.urlencoded({
+               extended: true
+          }));
+          server.use(express.json());
+
+          // Use React application on server
+          server.get("*", (req, res) => {
+               // Parse url param, slashesDenoteHost
+               const parsedUrl = parse(req.url, true);
+               const {
+                    pathname,
+                    query = {}
+               } = parsedUrl;
+
+               /**
+                * Pull in front end routes, and check request against those routes
+                */
+               const route = routes[pathname];
+               if (route) {
+                    return app.render(req, res, route.page, query);
+               }
+               return handle(req, res);
+          });
+
+          // Connect to DB
+          server.listen(PORT, err => {
+               if (err) throw err;
+               console.log(`> Ready on ${PORT}`);
+          });
+     })
+     .catch(ex => {
+          // Exit if there is an exception
+          console.error(ex.stack);
+          process.exit(1);
+     });
